5c533c689124a4c30caa8c79fbcd8383d5b9fb1f
yuchunlei <yuchunlei@yuchunlei-HP.sino-life.com>
2017-01-13 14:49:44 +0800

    upate Main

--- src/main/java/com/yucl/log/handle/async/Main.java
+++ src/main/java/com/yucl/log/handle/async/Main.java
@@ -3,16 +3,14 @@ package com.yucl.log.handle.async;
 public class Main {
 
 	public static void main(String[] args) {
-	/*	AccLogConsumer acclogConsumer = new AccLogConsumer("acclog");
+		AccLogConsumer acclogConsumer = new AccLogConsumer("acclog");
 		acclogConsumer.start();
 		AppLogConsumer applogConsumer = new AppLogConsumer("applog");
 		applogConsumer.start();
 		ContainerLogConsumer containerlogConsumer = new ContainerLogConsumer("container");
-		containerlogConsumer.start();*/
-		
-		 SysLogConsumer syslogConsumer = new SysLogConsumer("hostsyslog");
-		 syslogConsumer.start();
-		 
+		containerlogConsumer.start();
+		SysLogConsumer syslogConsumer = new SysLogConsumer("hostsyslog");
+		syslogConsumer.start();
 
 	}
 
