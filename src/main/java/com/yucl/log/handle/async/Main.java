f1ab8c77661367d5bba0a077eab72ce1550b95a2
yuchunlei <yuchunlei@yuchunlei-HP.sino-life.com>
2017-01-20 11:33:57 +0800

    refector

--- src/main/java/com/yucl/log/handle/async/Main.java
+++ src/main/java/com/yucl/log/handle/async/Main.java
@@ -1,15 +1,40 @@
 package com.yucl.log.handle.async;
 
-public class Main {
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
-	public static void main(String[] args) {
-		AccLogConsumer acclogConsumer = new AccLogConsumer("acclog");
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+	private static final Logger logger = LoggerFactory.getLogger(Main.class);
+	
+	public static void main(String[] args) {		
+		ThreadPoolExecutor pool = new ThreadPoolExecutor(Runtime.getRuntime().availableProcessors(), Runtime.getRuntime().availableProcessors(), 10000L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(10000));
+			pool.setRejectedExecutionHandler(new RejectedExecutionHandler() {
+
+				@Override
+				public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+					if (!executor.isShutdown()) {
+						logger.warn("executes task r in the caller's thread " + r.toString());
+						r.run();
+					} else {
+						logger.warn("the executor has been shut down, the task is discarded " + r.toString());
+					}
+
+				}
+
+			});
+			
+		LogConsumer acclogConsumer = new AccLogConsumer("acclog",pool);
 		acclogConsumer.start();
-		AppLogConsumer applogConsumer = new AppLogConsumer("applog");
+		LogConsumer applogConsumer = new AppLogConsumer("applog",pool);
 		applogConsumer.start();
-		ContainerLogConsumer containerlogConsumer = new ContainerLogConsumer("container");
+		LogConsumer containerlogConsumer = new ContainerLogConsumer("container",pool);
 		containerlogConsumer.start();
-		SysLogConsumer syslogConsumer = new SysLogConsumer("hostsyslog");
+		LogConsumer syslogConsumer = new SysLogConsumer("hostsyslog",pool);
 		syslogConsumer.start();
 
 	}
--- src/main/java/com/yucl/log/handle/async/SysLogConsumer.java
+++ src/main/java/com/yucl/log/handle/async/SysLogConsumer.java
@@ -1,101 +1,22 @@
 
 package com.yucl.log.handle.async;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.AsynchronousFileChannel;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ThreadPoolExecutor;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.jayway.jsonpath.DocumentContext;
 
-import com.jayway.jsonpath.JsonPath;
-
-import kafka.consumer.ConsumerConfig;
-import kafka.consumer.ConsumerIterator;
-import kafka.consumer.KafkaStream;
-import kafka.javaapi.consumer.ConsumerConnector;
-
-public class SysLogConsumer extends Thread {
-	private final ConsumerConnector consumer;
-	private final String topic;
-	private static ConcurrentHashMap<String, ChannelWrapper> channels = new ConcurrentHashMap<>();
-	private static final Logger logger = LoggerFactory.getLogger(SysLogConsumer.class);
+public class SysLogConsumer extends LogConsumer {	
+	
+	public SysLogConsumer(String topic, ThreadPoolExecutor threadPoolExecutor) {
+		super(topic, threadPoolExecutor);		
+	}
 
 	public SysLogConsumer(String topic) {
-		consumer = kafka.consumer.Consumer.createJavaConsumerConnector(createConsumerConfig());
-		this.topic = topic;
+		super(topic);
 	}
 
-	private static ConsumerConfig createConsumerConfig() {
-		Properties props = new Properties();
-		props.put("zookeeper.connect", KafkaProperties.zkConnect);
-		props.put("group.id", KafkaProperties.groupId);
-		props.put("zookeeper.session.timeout.ms", "4000");
-		props.put("zookeeper.sync.time.ms", "200");
-		props.put("auto.commit.interval.ms", "1000");
-
-		return new ConsumerConfig(props);
-
-	}
-
-	public void run() {
-		ChannelCloseThread channelCloseThread = new ChannelCloseThread(channels);
-		channelCloseThread.start();
-		String osName = System.getProperty("os.name");
-		String rootDir = "";
-		if (osName.indexOf("Window") > -1) {
-			rootDir = "c:";
-		}
-		Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
-		topicCountMap.put(topic, new Integer(1));
-		Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
-		KafkaStream<byte[], byte[]> stream = consumerMap.get(topic).get(0);
-		ConsumerIterator<byte[], byte[]> it = stream.iterator();
-		while (it.hasNext()) {
-			String msg = new String(it.next().message());
-			if (logger.isDebugEnabled()) {
-				logger.debug(msg);
-			}
-			try {
-				Path path = FileSystems.getDefault().getPath("", rootDir + JsonPath.read(msg, "$.path"));
-				String rawMsg = JsonPath.read(msg, "$.message");
-				ChannelWrapper channelWrapper = channels.get(path.toString());
-				if (channelWrapper == null) {
-					synchronized (channels) {
-						channelWrapper = channels.get(path);
-						if (channelWrapper == null) {
-							if (!Files.exists(path.getParent())) {
-								Files.createDirectories(path.getParent());
-							}
-							long fileLength= new File(path.toString()).length();
-							AsynchronousFileChannel asyncFile = AsynchronousFileChannel.open(path,
-									StandardOpenOption.WRITE, StandardOpenOption.CREATE);
-							channelWrapper = new ChannelWrapper(asyncFile,fileLength);
-							channels.put(path.toString(), channelWrapper);
-						}
-					}
-				}
-				synchronized (channelWrapper) {
-					byte[] bytes = (rawMsg + "\n").getBytes();
-					channelWrapper.getFileChannel().write(ByteBuffer.wrap(bytes), channelWrapper.getPos());
-					channelWrapper.setPos(channelWrapper.getPos() + bytes.length);
-					channelWrapper.setLastWriteTime(System.currentTimeMillis());
-				}
-			} catch (IOException e) {
-				logger.error(msg, e);
-			}
-
-		}
-
+	@Override
+	public String buildFilePathFromMsg(DocumentContext msgJsonContext, String rootDir) {		
+		return rootDir + msgJsonContext.read("$.path", String.class);
 	}
 }
