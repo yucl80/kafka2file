59bb209c676e75d07b3c1abcd34f2f608f3082f6
yuchunlei <yuchunlei@yuchunlei-HP.sino-life.com>
2017-01-20 08:53:59 +0800

    add customer thread pool for AsynchronousFileChannel

---
+++ src/test/java/com/learn/kafka/C.java
@@ -0,0 +1,52 @@
+package com.learn.kafka;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class C {
+
+	public static void main(String[] args) {
+		ThreadPoolExecutor pool = new ThreadPoolExecutor(5, 5, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(2500));
+		pool.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+		
+		
+		Path path= FileSystems.getDefault().getPath("", "a.txt");
+		long len = 0;
+		try {
+			AsynchronousFileChannel asyncFile = AsynchronousFileChannel.open(path,
+					new HashSet<StandardOpenOption>(Arrays.asList(StandardOpenOption.WRITE, 
+                            StandardOpenOption.CREATE)), pool);
+			int bs = 1024 * 64 ;
+			byte[] bytes=new byte[bs];
+			for(int i=0;i<bs;i++){
+				bytes[i] = 'a';
+			}
+			long begin=System.currentTimeMillis();
+			for(int i=0;i<100000;i++){
+				asyncFile.write(ByteBuffer.wrap(bytes), len);
+				len += bytes.length;
+			}
+			long end=System.currentTimeMillis();
+			System.out.println(len/1024/1024 +"M : "+len/1024/1024*1000/(end-begin)+ "M/s");
+			begin=System.currentTimeMillis();
+			asyncFile.force(false);
+			asyncFile.close();
+			end=System.currentTimeMillis();
+			System.out.println("close time:"+ (end-begin));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
