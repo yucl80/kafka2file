094ede1391286d72f7367fa7b20da8456e0c01ee
yuchunlei <yuchunlei@yuchunlei-HP.sino-life.com>
2017-01-20 09:16:54 +0800

    update thread pool

--- src/test/java/com/learn/kafka/C.java
+++ src/test/java/com/learn/kafka/C.java
@@ -9,39 +9,61 @@ import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.yucl.log.handle.async.AppLogConsumer;
+
 public class C {
+	private static final Logger logger = LoggerFactory.getLogger(AppLogConsumer.class);
 
 	public static void main(String[] args) {
-		ThreadPoolExecutor pool = new ThreadPoolExecutor(5, 5, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(2500));
-		pool.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-		
-		
-		Path path= FileSystems.getDefault().getPath("", "a.txt");
+		ThreadPoolExecutor pool = new ThreadPoolExecutor(5, 5, 0L, TimeUnit.MILLISECONDS,
+				new LinkedBlockingQueue<Runnable>(2500));
+		// pool.setRejectedExecutionHandler(new
+		// ThreadPoolExecutor.CallerRunsPolicy());
+		pool.setRejectedExecutionHandler(new RejectedExecutionHandler() {
+
+			@Override
+			public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {				
+				if (!executor.isShutdown()) {
+					logger.warn("executes task r in the caller's thread " + r.toString());
+					r.run();
+				}else{
+					logger.warn("the executor has been shut down, the task is discarded " + r.toString());
+				}
+
+			}
+
+		});
+
+		Path path = FileSystems.getDefault().getPath("", "a.txt");
 		long len = 0;
 		try {
 			AsynchronousFileChannel asyncFile = AsynchronousFileChannel.open(path,
-					new HashSet<StandardOpenOption>(Arrays.asList(StandardOpenOption.WRITE, 
-                            StandardOpenOption.CREATE)), pool);
-			int bs = 1024 * 64 ;
-			byte[] bytes=new byte[bs];
-			for(int i=0;i<bs;i++){
+					new HashSet<StandardOpenOption>(Arrays.asList(StandardOpenOption.WRITE, StandardOpenOption.CREATE)),
+					pool);
+			int bs = 1024 * 64;
+			byte[] bytes = new byte[bs];
+			for (int i = 0; i < bs; i++) {
 				bytes[i] = 'a';
 			}
-			long begin=System.currentTimeMillis();
-			for(int i=0;i<100000;i++){
+			long begin = System.currentTimeMillis();
+			for (int i = 0; i < 100000; i++) {
 				asyncFile.write(ByteBuffer.wrap(bytes), len);
 				len += bytes.length;
 			}
-			long end=System.currentTimeMillis();
-			System.out.println(len/1024/1024 +"M : "+len/1024/1024*1000/(end-begin)+ "M/s");
-			begin=System.currentTimeMillis();
+			long end = System.currentTimeMillis();
+			System.out.println(len / 1024 / 1024 + "M : " + len / 1024 / 1024 * 1000 / (end - begin) + "M/s");
+			begin = System.currentTimeMillis();
 			asyncFile.force(false);
 			asyncFile.close();
-			end=System.currentTimeMillis();
-			System.out.println("close time:"+ (end-begin));
+			end = System.currentTimeMillis();
+			System.out.println("close time:" + (end - begin));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
