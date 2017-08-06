7d3fd451f78180fd7982588d2dcfc0eecc2db177
yucl80 <ycl123>
2017-08-06 21:07:43 +0800

    commit

--- src/main/java/com/yucl/log/handle/async/Main.java
+++ src/main/java/com/yucl/log/handle/async/Main.java
@@ -11,7 +11,7 @@ public class Main {
 	private static final Logger logger = LoggerFactory.getLogger(Main.class);
 	
 	public static void main(String[] args) {		
-		ThreadPoolExecutor pool = new ThreadPoolExecutor(40, 80, 10000L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(10000));
+		ThreadPoolExecutor pool = new ThreadPoolExecutor(Runtime.getRuntime().availableProcessors(), Runtime.getRuntime().availableProcessors()*2, 10000L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(10000));
 			pool.setRejectedExecutionHandler((r, executor) -> {
                 if (!executor.isShutdown()) {
                     logger.warn("executes task r in the caller's thread " + r.toString());
